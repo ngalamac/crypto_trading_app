ccf66dda93ea7f5304afa786e1fd029d1ccf0819
NGALA MAC <macngala4@gmail.com>
2024-05-10 09:29:37 +0100

    Update repo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     # Validations
-  validates :phone_number, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-  validates :invitation_code, presence: true
+  #validates :phone_number, presence: true, uniqueness: true
+  #validates :password_digest, presence: true
+  #validates :invitation_code, presence: true
 
   # Associations
   has_many :verification_codes
